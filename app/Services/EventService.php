73554ed04b13797f5e43d0740b72519922ffc6ad
Okumura0903 <127096042+Okumura0903@users.noreply.github.com>
2023-05-22 20:29:26 +0900

    event dummy 修正

--- app/Services/EventService.php
+++ app/Services/EventService.php
@@ -27,4 +27,32 @@ class EventService
             ->count();
     }
 
+    public static function getWeekEvents($startDate,$endDate){
+        //予約テーブルからイベントごとの合計人数でテーブルを作る
+        $reservedPeople=DB::table('reservations')->select('event_id',DB::raw('sum(number_of_people) as number_of_people'))
+        ->whereNull('canceled_date')
+        ->groupBy('event_id');//DB::rawで生のSQLを書ける
+
+        //内部結合・・合計人数がない場合データが表示されない：joinSub
+        //外部結合・・合計人数がない場合、nullとして表示される：leftJoinSub
+        // +"id": 1
+        // +"name": "青山 美加子"
+        // +"information": "を解とけいやの店にはなんと立ちどま向むかしだのそとを思いだいもいいのがつまりもう咽喉のどい近眼鏡きんがたずねました。（この頁ページ一つずつ集あつまみ、掌てのぞきこうね」と叫さけびましたら、それでも着ついているかとおりたいてしました。「ありません。りんごを見ました。それにさっきかんしゅうに見えないかけて、この辺へんじまい ▶"
+        // +"max_people": 17
+        // +"start_date": "2023-05-01 09:01:37"
+        // +"end_date": "2023-05-01 10:01:37"
+        // +"is_visible": 1
+        // +"created_at": "2023-05-21 20:02:15"
+        // +"updated_at": "2023-05-21 20:02:15"
+        // +"event_id": 1
+        // +"number_of_people": "8"
+
+        return DB::table('events')
+            ->leftJoinSub($reservedPeople,'reservedPeople',function($join){//元のテーブルにnumber_of_peopleを追加する
+                $join->on('events.id','=','reservedPeople.event_id');//イベントテーブルのidと↑の予約人数テーブルのidで結合
+            })
+            ->whereBetween('start_date',[$startDate,$endDate])
+            ->orderBy('start_date','asc')
+            ->get();
+    }
 }
